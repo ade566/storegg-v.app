38344a559d66bd288929d6e4ffeef409b64ac1ba
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2023-06-08 04:48:06 +0700

    import bootstrap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
-// import '../styles/utilities.css'
-// import '../styles/homepage.css'
-
 import { Inter } from 'next/font/google'
 import Script from 'next/script'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import '../styles/utilities.css'
+import '../styles/homepage.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,12 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* boostrap */}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossOrigin="anonymous" />
-
-        <link href="/utilities.css" rel="stylesheet" />
-        <link href="/homepage.css" rel="stylesheet" />
-
         {/* google font */}
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
