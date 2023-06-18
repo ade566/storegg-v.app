399e54933335c5461ddefcd6d3e0df5d7dd6b64b
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2023-06-18 09:50:57 +0700

    success sign up page

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import '../styles/complete-checkout.css'
 import '../styles/sign-in.css'
 import '../styles/sign-up.css'
 import '../styles/sign-up-photo.css'
+import '../styles/sign-up-success.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
