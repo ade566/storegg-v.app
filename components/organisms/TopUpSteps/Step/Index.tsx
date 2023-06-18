ac214fe710614be9c3abffb6def83cf782eb7066
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2023-06-18 13:13:26 +0700

    component top up steps

---
+++ components/organisms/TopUpSteps/Step/Index.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+interface StepProps {
+  title: string;
+  description: string;
+  image: string;
+}
+
+export function Step(props: StepProps) {
+
+  const {
+    title, 
+    description, 
+    image
+  } = props
+
+  return (
+    <div className="card feature-card border-0">
+      <Image src={image} width={80} height={80} alt="" className="mb-30" />
+      <p className="fw-semibold text-2xl mb-2 color-palette-1">{title}</p>
+      <p className="text-lg color-palette-1 mb-0">{description}</p>
+    </div>
+  )
+}
---
+++ components/organisms/TopUpSteps/index.tsx
@@ -0,0 +1,23 @@
+import { Step } from "./Step/Index";
+
+export function TopUpSteps() {
+  return (
+    <section id="feature" className="feature pt-50 pb-50">
+      <div className="container-fluid">
+        <h2 className="text-4xl fw-bold color-palette-1 text-center mb-30">It’s Really That<br /> Easy to Win the Game
+        </h2>
+        <div className="row gap-lg-0 gap-4" data-aos="fade-up">
+          <div className="col-lg-4">
+            <Step title="1. Start" description="Pilih salah satu game yang ingin kamu top up" image="/img/steps/1.svg" />
+          </div>
+          <div className="col-lg-4">
+            <Step title="2. Fill Up" description="Top up sesuai dengan nominal yang sudah tersedia" image="/img/steps/2.svg" />
+          </div>
+          <div className="col-lg-4">
+            <Step title="3. Be a Winner" description="Siap digunakan untuk improve permainan kamu" image="/img/steps/3.svg" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
