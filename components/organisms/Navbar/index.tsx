86158c47f0d59b93e14fa7e64af166de151c7916
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2023-06-18 13:17:12 +0700

    LINK on logo navbar

--- components/organisms/Navbar/index.tsx
+++ components/organisms/Navbar/index.tsx
@@ -7,9 +7,9 @@ export function Navbar() {
     <section>
       <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" href="/">
             <Image src="/icon/logo.svg" width={60} height={60} alt="" />
-          </a>
+          </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
